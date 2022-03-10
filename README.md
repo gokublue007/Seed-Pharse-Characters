164f65e71ee5b572d2f0ad83107f37254ff3ab1f
Elias Vasquez <eliasvasquez7@gmail.com>
2022-03-09 22:14:45 -0700

    Added Java to help create complex passwords

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Seed-Pharse-Characters-Generator
+
+![Project Image](../Seed-Pharse-Characters-Generator/Assest/Screen Shot 2022-03-09 at 10.07.27 PM.pdf)
+
+> Creating a codebase that follows accessibility standards so their own site is optimized for search engines.
+
+---
+
+### Table of Contents
+
+- [Description](#description)
+
+---
+
+## Description
+I will be able to present a series of prompts for password criteria
+prompted for password criteria. I'll be able to select which criteria to include in the password
+prompted for the length of the password and choose a length of at least 8 characters and no more than 128 characters. They must include in the password and confirm whether or not to include lowercase, uppercase, numeric, and/or special characters.
+When my input is able to be validated and at least one character type should be selected.
+A password is generated that matches the selected criteria and it will generate a passwork.
+
+#### Technologies
+
+- Html
+- CSSs
+- Java Script
+
+[Back To The Top](#read-me-template)
--- script.js
+++ script.js
@@ -1,22 +1,71 @@
-// Assignment Code
+// Created geneateBtn and querySelcetor
 var generateBtn = document.querySelector("#generate");
 
+//Added Varibles to be able to generate password
+
+var lowerCase = "abcdefghigklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLONOPQRSTUVWXYZ";
+var numbersCase = "0123456789";
+var specialCharactersCase = "!@#$%^&*()?><.,;:[{}]";
+
+// Added Functions/confirms/for and var to be able to create password!
+
 function generatePassword() {
-  window.prompt("How many Characters would you like your passwork to have?");
-  if (!userChoice) {
-    return;
+  var finalPassword = "";
+  var passwordLength = parseInt(
+    prompt("How many Characters would you like your password to have?")
+  );
+  console.log(passwordLength);
+
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    var isLowerCase = confirm(
+      "Do you want to create lowercase in your password."
+    );
+    var isUpperCase = confirm(
+      "Do you want to create uppercase in your password."
+    );
+    var isNumbersCase = confirm(
+      "Do you want to create numeric case in your password."
+    );
+    var isSpecialCharactersCase = confirm(
+      "Do you want to special characters numeric case in your password."
+    );
+    for (var i = 0; i < passwordLength; i++) {
+      if (isLowerCase === true && finalPassword.length < passwordLength) {
+        finalPassword = finalPassword + random(lowerCase);
+      }
+      if (isUpperCase === true && finalPassword.length < passwordLength) {
+        finalPassword = finalPassword + random(upperCase);
+      }
+      if (isNumbersCase === true && finalPassword.length < passwordLength) {
+        finalPassword = finalPassword + random(numbersCase);
+      }
+      if (isSpecialCharactersCase === true && finalPassword.length < passwordLength) {
+        finalPassword = finalPassword + random(specialCharactersCase);
+      }
+    }
+    
+  } else {
+    alert("Invaild entry charatcers between 8-128. ");
   }
+
+  return finalPassword;
 }
 
-
-// Write password to the #password input
+// Wrote password to the #password input unit
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+function random(array) {
+  var randomIndex = Math.floor(Math.random() * array.length);
+  var randomElement = array[randomIndex];
+
+  return randomElement;
+}
+
+// Added event listener to generate button
 generateBtn.addEventListener("click", writePassword);
